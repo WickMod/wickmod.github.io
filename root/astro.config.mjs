8ab076c06aed0f95920d2d985526ce90bd4ddd87
WickMod <73618856+WickMod@users.noreply.github.com>
2024-02-20 01:27:59 -0500

    .

--- root/astro.config.mjs
+++ root/astro.config.mjs
@@ -3,9 +3,18 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`
+const LIVE_URL = 'https://wickmod.github.io'
+let BASE_URL = LOCALHOST_URL
+
+if (isBuild){
+	BASE_URL=LIVE_URL;
+}
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://wickmod.github.io',
+	server: {port:SERVER_PORT},
+	site: BASE_URL,
 	integrations: [mdx(), sitemap()],
-	base:"../BUILD",
 });
